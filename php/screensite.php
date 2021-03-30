7aaef0718a5d53ee8f9421417c151477a192a4fe
EisaaKu <81249384+EisaaKu@users.noreply.github.com>
2021-03-30 16:49:39 +0200

    Add files via upload

---
+++ php/screensite.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<title>0verFly-PhotoFaction</title>
+	<link rel="stylesheet" type="text/css" href='site.css.';/>
+</head>
+	<link rel="icon" type="image/ico" href="logo.ico"> <link rel="shortcut icon" type="image/x-icon"/>
+<body class="deuxieme">
+	
+	<section>
+		<h1>
+			<div class="centerBox">
+  
+  <div class="categoryWrapper">
+    <h1>AverFight/PvPFaction</h1>
+    <button>
+      <span>
+        <span>
+          <span data-attr-span="OverFly">
+            OverFly
+          </span>
+        </span>
+      </span>
+    </button>
+  </div>
+  
+</div>
+			<p class="screen1">Voici les images de nos farms/spawners/bases:</p>
+			<p class="lignescreen"> -Spawners:</p> 
+			<img src="spawners.png"  width="50%" >
+			<p class="melon"> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> -Farms à Melons:</p>
+			<img class="melon" src="melon.png"  width="50%" >
+			<p class="cactus"> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>-Farms à Cactus (en cours):</p>
+			<img class="cactus" src="cactus.png" width="50%" >
+			<p class="bk"> -Notre Bunker:</p> 
+			<br> <br> <br><img class="bk" src="bk.png"  width="50%" >
+		<h2>
+			<br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
+			<p class="screen2">Voici les images de la fac en plein PvP: </p> 
+			<p class="lignescreen"> -On drop un full Zarco !</p>
+			<img class="pvp" src="drop.jpg"  width="50%" >
+			<p class="grospvp"> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> -Ils sont combien au juste ?</p>
+			<img class="grospvp" src="grospvp.jpg"  width="50%" >
+			<p class="lignescreen"> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> 
+			 -On tue un full Zarco a cause d'une porte ?!</p>
+			<img src="porte.jpg" width="50%">
+
+		</h2>
+		<h3>
+			<p class="screen3">Voici les images de la fac au complet:</p> 
+			<p class="lignescreen"> -Full Zarco !</p>
+			<img class="pvp" src="pvp.jpg"  width="50%" >
+			<p class="invincible"> <br> <br> <br> <br> <br> <br> <br> <br> <br> -On est invincible !</p> 
+			<img class="invincible" src="invincible.jpg" width="50%">
+			<p class="lignescreen"> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>-Je suis sauver !</p>
+			<img src="sauver.jpg" width="50%">
+			
+		</h3>
+		<br> <br> <br> <br> <br> <br> <br> <br>
+		<h4>
+			<div class="svg-wrapper">
+  			<svg height="60" width="500" xmlns="http://www.w3.org/2000/svg">
+    		<rect class="shape1" height="60" width="500" />
+    		<rect class="shape2" height="60" width="500" />
+    		<div class="text">Merci pour votre visite</div>
+  			</svg>
+			</div>	
+		</h4>
+
+	</section>
+
+</body>
+</html>
